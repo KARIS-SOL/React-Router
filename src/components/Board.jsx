ed33df2ca03e5c938ed17ae102fd341d16b4a1e7
Karis-Sol <karis.baik1124@gmail.com>
2023-04-03 13:02:58 +0900

    Reactrouter 수업

---
+++ src/components/Board.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Board() {
+  return <h1>Board 페이지 입니다</h1>;
+}
---
+++ src/components/Profile.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Profile() {
+  return <h1>Profile 페이지 입니다</h1>;
+}
